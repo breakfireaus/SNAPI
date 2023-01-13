152d72a87f6b006b45038af9ace06ca5eb180d5f
breakfireaus <myoungerc@hotmail.com>
2023-01-14 10:05:04 +1100

    forgot to save server.js and now saved and pushed up.

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// public folder for later
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/social-network',
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
