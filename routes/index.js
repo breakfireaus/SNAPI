5853d3d2252ad4640173b7b95a8726c890b22e56
breakfireaus <myoungerc@hotmail.com>
2023-01-14 12:46:51 +1100

    changed to h2 index.js

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send(`<h2>You are searching for something that has not been created. 404 Error!</h2>`);
+});
+
+module.exports = router;
