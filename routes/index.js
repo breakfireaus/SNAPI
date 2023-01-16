664bc12c462c605f6023dc98651dc7050869a687
breakfireaus <myoungerc@hotmail.com>
2023-01-16 22:20:11 +1100

    updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send(`<h2>You are searching for something that has not been created. 404 Error!</h2>`);
+  res
+    .status(404)
+    .send(
+      `<h2>You are searching for something that has not been created. 404 Error!</h2>`
+    );
 });
 
 module.exports = router;
